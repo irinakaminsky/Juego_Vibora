a3b11cd6683eae7e49beff8c2c74119fddabf99d
irinakaminsky <a00827550@itesm.mx>
2021-06-29 22:58:10 -0500

    Update CodigoVibora.py

--- CodigoVibora.py
+++ CodigoVibora.py
@@ -2,18 +2,30 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
-food = vector(0, 0)
+food = [vector(0, 0)]
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-
-def change(x, y):
-    "Change snake direction."
-    aim.x = x
-    aim.y = y
 
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
+
+def movefood():
+    "Move snake forward one segment."
+    foodhead = food[-1].copy()
+    foodhead.move(aim)
+
+    if not inside(foodhead) or foodhead in food:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    food.append(foodhead)
+    
+def change(x, y):
+    "Change snake direction."
+    aim.x = x
+    aim.y = y
 
 def move():
     "Move snake forward one segment."
@@ -26,20 +38,22 @@ def move():
         return
 
     snake.append(head)
-
-    if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
-    else:
-        snake.pop(0)
+# 
+#      if head == foodhead:
+#         print('Snake:', len(snake))
+#         food.x = randrange(-15, 15) * 10
+#         food.y = randrange(-15, 15) * 10
+#     else:
+#         snake.pop(0)
 
     clear()
 
     for body in snake:
         square(body.x, body.y, 9, 'black')
+        
+    for body in food:
+        square(bodyf.x, bodyf.y, 9, 'green')
 
-    square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
 
@@ -51,5 +65,6 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+movefood()
 move()
 done()
